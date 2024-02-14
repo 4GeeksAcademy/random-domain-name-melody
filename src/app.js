5b8f1e30aec3ca4f801c7e41e90f1e7a14441e34
Melody Callejas <154374405+melodycious@users.noreply.github.com>
2024-02-14 20:20:06 +0000

    codigo dominio añadido

--- src/app.js
+++ src/app.js
@@ -7,5 +7,58 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  console.log("Hello darling");
 };
+
+let domainName = [];
+
+let article = ["un", "mi"];
+
+let fullName = ["dominio", "sitio", "espacio"];
+
+let adjective = ["genial", "asombroso", "exclusivo", "increible"];
+
+let web = [".es", ".com", ".net"];
+
+/* let pronounIndex = Math.floor(Math.random() * pronoun.length);
+  let nameIndex = Math.floor(Math.random() * name.length);
+  let adjetiveIndex = Math.floor(Math.random() * adjective.length);
+  let webIndex = Math.floor(Math.random() * web.length);
+
+  return (
+    pronoun[pronounIndex] +
+    name[nameIndex] +
+    adjective[adjetiveIndex] +
+    web[webIndex]
+  ); */
+/* for (let article of articles) {
+  for (let name of names) {
+    for (let adjective of adjectives) {
+      for (let web of webs) {
+        console.log("${article}${name}${adjective}${web}");
+      }
+    }
+  }
+}
+console.log(generateDomain()); */
+
+for (let i = 0; i < article.length; i++) {
+  for (let j = 0; j < fullName.length; j++) {
+    for (let k = 0; k < adjective.length; k++) {
+      for (let l = 0; l < web.length; l++) {
+        domainName.push(
+          "  " + article[i] + fullName[j] + adjective[k] + web[l]
+        );
+      }
+    }
+  }
+}
+console.log(domainName);
+
+let generateDomains = () => {
+  return domainName;
+};
+
+document.querySelector("#boton").addEventListener("click", function() {
+  document.getElementById("listaDominios").innerHTML = generateDomains();
+});
