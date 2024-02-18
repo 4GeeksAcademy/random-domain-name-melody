ae0d97eb6e74e2a2c38515bad1450197e60de3dd
Melody Callejas <154374405+melodycious@users.noreply.github.com>
2024-02-18 20:59:03 +0000

    lista dominios funcionando

--- src/app.js
+++ src/app.js
@@ -10,55 +10,27 @@ window.onload = function() {
   console.log("Hello darling");
 };
 
-let domainName = [];
-
 let article = ["un", "mi"];
 
 let fullName = ["dominio", "sitio", "espacio"];
 
 let adjective = ["genial", "asombroso", "exclusivo", "increible"];
 
-let web = [".es", ".com", ".net"];
-
-/* let pronounIndex = Math.floor(Math.random() * pronoun.length);
-  let nameIndex = Math.floor(Math.random() * name.length);
-  let adjetiveIndex = Math.floor(Math.random() * adjective.length);
-  let webIndex = Math.floor(Math.random() * web.length);
-
-  return (
-    pronoun[pronounIndex] +
-    name[nameIndex] +
-    adjective[adjetiveIndex] +
-    web[webIndex]
-  ); */
-/* for (let article of articles) {
-  for (let name of names) {
-    for (let adjective of adjectives) {
-      for (let web of webs) {
-        console.log("${article}${name}${adjective}${web}");
+function DomainGenerator(ar, n, ad) {
+  for (let a = 0; a < ar.length; a++) {
+    for (let b = 0; b < n.length; b++) {
+      for (let c = 0; c < ad.length; c++) {
+        let nameList = document.createTextNode(ar[a] + n[b] + ad[c] + ".com");
+        let listofnames = document.getElementById("DomainList");
+        let nameline = document.createElement("div");
+        nameline.className = "alert alert-info";
+        listofnames.appendChild(nameline);
+        nameline.appendChild(nameList);
       }
     }
   }
 }
-console.log(generateDomain()); */
-
-for (let i = 0; i < article.length; i++) {
-  for (let j = 0; j < fullName.length; j++) {
-    for (let k = 0; k < adjective.length; k++) {
-      for (let l = 0; l < web.length; l++) {
-        domainName.push(
-          "  " + article[i] + fullName[j] + adjective[k] + web[l]
-        );
-      }
-    }
-  }
-}
-console.log(domainName);
-
-let generateDomains = () => {
-  return domainName;
-};
-
-document.querySelector("#boton").addEventListener("click", function() {
-  document.getElementById("listaDominios").innerHTML = generateDomains();
+document.querySelector("#buttonDomain").addEventListener("click", function() {
+  DomainGenerator(article, fullName, adjective);
+  document.querySelector("#buttonDomain").style.display = "none";
 });
